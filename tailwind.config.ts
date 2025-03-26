9ddbc43714fed6a7f0b248550c5f81bb292f393b
wesley <wesleylwu@gmail.com>
2025-03-25 18:07:09 -0700

    remove some colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,15 +5,11 @@ module.exports = {
     extend: {
       colors: {
         ptso: {
-          "white-100": "#FFF4F4",
           "blue-100": "#DAEDFE",
           "blue-200": "#B6CDE4",
-          "blue-300": "#80AED9",
-          "blue-400": "#151539",
           "purple-100": "#4545A4",
           "purple-200": "#7676CC",
           "pink-100": "#D092AA",
-          "pink-200": "#B06985",
           "pink-300": "#A85173",
         },
         fontFamily: {
