b36b82304f52bf9d2e9175cc0ab42e09e78d64ba
Briana Garcia <briana_garcia_@outlook.com>
2025-04-07 13:54:23 -0700

    Fix format

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,17 +7,17 @@ const Navbar = () => {
   return (
     <div className="text-ptso-purple-accent">
       <div className="flex items-center justify-between px-6 py-3">
-          <Link href="/">
-            <Image src={ptsoLogo} alt="PTSO Logo" height={70} width={49} />
-          </Link>
-          <div className="text-3xl -ml-11">PTSO</div>
-          <div className="flex gap-11 text-xl font-beVietnamPro w-3xl justify-start">
-            {navigations.map(({ link, name }, index) => (
-              <Link href={link} key={index}>
-                {name}
-              </Link>
-            ))}
-          </div>
+        <Link href="/">
+          <Image src={ptsoLogo} alt="PTSO Logo" height={70} width={49} />
+        </Link>
+        <div className="-ml-11 text-3xl">PTSO</div>
+        <div className="font-beVietnamPro flex w-3xl justify-start gap-11 text-xl">
+          {navigations.map(({ link, name }, index) => (
+            <Link href={link} key={index}>
+              {name}
+            </Link>
+          ))}
+        </div>
         <div className="pl-48">JOIN US</div>
       </div>
       <div className="bg-ptso-blue-accent mr-3 ml-3 h-1 rounded-2xl"></div>
