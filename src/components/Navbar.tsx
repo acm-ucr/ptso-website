1f303fcfaf1244bae929394e95cc757554cb8629
Briana Garcia <briana_garcia_@outlook.com>
2025-04-07 13:52:50 -0700

    Small fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,29 +3,22 @@ import Link from "next/link";
 import { navigations } from "@/data/NavbarLinks";
 import ptsoLogo from "@/public/ptsoLogo.webp";
 
-type Navigation = {
-  name: string;
-  href: string;
-};
-
 const Navbar = () => {
   return (
-    <div className="text-ptso-purple-accent font-beVietnamPro">
+    <div className="text-ptso-purple-accent">
       <div className="flex items-center justify-between px-6 py-3">
-        <div className="flex items-center gap-5 text-3xl">
           <Link href="/">
             <Image src={ptsoLogo} alt="PTSO Logo" height={70} width={49} />
           </Link>
-          <p className="font-average text-3xl">PTSO</p>
-          <div className="flex gap-11 pl-20 text-xl">
-            {navigations.map((item: Navigation, index: number) => (
-              <Link href={item.href} key={index}>
-                {item.name}
+          <div className="text-3xl -ml-11">PTSO</div>
+          <div className="flex gap-11 text-xl font-beVietnamPro w-3xl justify-start">
+            {navigations.map(({ link, name }, index) => (
+              <Link href={link} key={index}>
+                {name}
               </Link>
             ))}
           </div>
-        </div>
-        JOIN US
+        <div className="pl-48">JOIN US</div>
       </div>
       <div className="bg-ptso-blue-accent mr-3 ml-3 h-1 rounded-2xl"></div>
     </div>
