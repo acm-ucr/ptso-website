277d13c93e4a8259d838907222ebc898dc142d4f
sidechara <creativenamegoeshere0@gmail.com>
2025-04-08 16:43:54 -0700

    Initial changes

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,18 +1,20 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-ptso-pink-primary flex w-3/4 items-center gap-1 justify-self-center p-10">
-      <div className="bg-ptso-blue-secondary w-1/2 p-5 text-left">
+    <div className="bg-ptso-pink-primary flex w-2/3 items-center gap-1 justify-self-center p-10 flex-col rounded-lg">
+      <div className="bg-ptso-blue-accent w-1/2 p-5 text-left">
         {props.text1}
       </div>
       <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-ptso-blue-secondary w-full p-5 text-left">
+        <div className="bg-ptso-blue-secondary w-full p-5 text-left rounded-full">
           props.text2
         </div>
-        <div className="bg-ptso-blue-secondary w-full p-5 text-right">
+        <div className="bg-ptso-blue-secondary w-full p-5 text-right rounded-full">
           {props.text2}
         </div>
       </div>
-      {/* Hint: Add another box/div here similarly to line 4 */}
+      <div className="bg-ptso-blue-secondary w-1/2 p-5 text-left">
+        {props.text1}
+      </div>
     </div>
   );
 };
