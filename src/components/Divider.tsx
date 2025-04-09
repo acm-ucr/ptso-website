e83a5d04fac2c71fcd3d4ff04a0fb0681c80a892
Rachel Chan <rchan2023@gmail.com>
2025-04-09 01:29:54 -0700

    created divider line

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,4 +1,6 @@
 const Divider = () => {
-  return <div className="">Divider</div>;
+  return (
+    <div className="border-ptso-blue-accent mx-auto w-49/50 rounded-xl border-t-3"></div>
+  );
 };
 export default Divider;
