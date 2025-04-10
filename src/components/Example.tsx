06a5daa8f1e75592779e2c8f4d304930eb77bd53
sidechara <creativenamegoeshere0@gmail.com>
2025-04-10 09:09:07 -0700

    More example changes

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,19 +1,19 @@
 const Example = (props: { text1: string; text2: string; text3: string }) => {
   return (
-    <div className="bg-ptso-pink-primary flex w-2/3 items-center gap-1 justify-self-center p-10 flex-col rounded-lg">
-      <div className="bg-ptso-blue-accent w-1/2 p-5 text-left">
+    <div className="bg-ptso-pink-primary flex w-2/3 items-center gap-13 justify-self-center p-10 flex-col rounded-lg">
+      <div className="bg-ptso-blue-primary w-1/2 p-6 text-center text-xl font-quicksand font-black">
         {props.text1}
       </div>
-      <div className="flex w-1/2 flex-row gap-1">
-        <div className="bg-ptso-blue-secondary w-full p-5 text-left rounded-full">
-          props.text2
+      <div className="flex w-1/2 flex-row gap-3">
+        <div className="bg-ptso-blue-secondary w-full p-6 text-center rounded-full text-xl font-quicksand font-black">
+          {props.text2}
         </div>
-        <div className="bg-ptso-blue-secondary w-full p-5 text-right rounded-full">
+        <div className="bg-ptso-blue-secondary w-full p-6 text-center rounded-full text-xl font-quicksand font-black">
           {props.text2}
         </div>
       </div>
-      <div className="bg-ptso-blue-secondary w-1/2 p-5 text-left">
-        {props.text1}
+      <div className="bg-ptso-blue-accent w-1/2 p-6 text-center text-white text-xl font-quicksand font-black">
+        {props.text3}
       </div>
     </div>
   );
