ff06051c42d171409733c737a5164a169d695f62
Rachel Chan <rchan2023@gmail.com>
2025-04-10 11:25:40 -0700

    fixed divider

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,6 +1,6 @@
 const Divider = () => {
   return (
-    <div className="border-ptso-blue-accent mx-auto w-49/50 rounded-xl border-t-3"></div>
+    <div className="border-ptso-blue-accent rounded-xl border-t-3"></div>
   );
 };
 export default Divider;
