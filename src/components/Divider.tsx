76d089cdd0dcd482ddb12cf167f894ac89dcf8b9
Rachel Chan <rchan2023@gmail.com>
2025-04-10 11:28:18 -0700

    fix

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -1,6 +1,4 @@
 const Divider = () => {
-  return (
-    <div className="border-ptso-blue-accent rounded-xl border-t-3"></div>
-  );
+  return <div className="border-ptso-blue-accent rounded-xl border-t-3"></div>;
 };
 export default Divider;
