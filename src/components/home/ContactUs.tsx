f64c7bc67e644ea6193129086f8629eae996c88d
xxiyun <wesleylwu@gmail.com>
2025-04-11 18:32:16 -0700

    add images

--- src/components/home/ContactUs.tsx
+++ src/components/home/ContactUs.tsx
@@ -1,4 +1,19 @@
+import Image from "next/image";
+import rectangle2 from "@/public/assets/home/background/rectangle2.svg";
+import vector6 from "@/public/assets/home/background/vector6.svg";
+import contactUs from "@/public/assets/home/contactUs.webp";
+
 const ContactUs = () => {
-  return <div className="">ContactUs</div>;
+  return (
+    <div>
+      <div className="absolute flex flex-row">
+        <Image src={contactUs} alt="Contact Us Hand" className="" />
+        <Image src={vector6} alt="Contact Us Blob" className="" />
+      </div>
+      <div className="">
+        <Image src={rectangle2} alt="Contact Us Wave" className="" />
+      </div>
+    </div>
+  );
 };
 export default ContactUs;
