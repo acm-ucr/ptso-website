1165cd03bdf29d89e5c934501335a3a56f8048cd
Briana Garcia <briana_garcia_@outlook.com>
2025-04-11 20:36:46 -0700

    Fix spacing for normal nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import ptsoLogo from "@/public/ptsoLogo.webp";
 const Navbar = () => {
   return (
     <div className="text-ptso-purple-accent relative">
-      <div className="flex items-center justify-between px-6 py-3">
+      <div className="flex items-center px-6 py-3">
         <Link href="/" className="flex w-1/5 items-center">
           <Image src={ptsoLogo} alt="PTSO Logo" height={70} width={49} />
           <div className="px-4 text-3xl">PTSO</div>
