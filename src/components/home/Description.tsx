8cca7a73e4eecfc37ba411ee03db8382c00c4b43
Yubin Zhen <yubinzhen@Yubins-MacBook-Pro.local>
2025-04-17 17:43:23 -0700

    fixed

--- src/components/home/Description.tsx
+++ src/components/home/Description.tsx
@@ -1,20 +1,16 @@
 const Description = () => {
   return (
-    <div className="flex justify-center">
-      <div className="text-ptso-purple-accent font-quicksand h-auto w-7xl text-center text-3xl">
-        <p>
-          We are committed to fostering a supportive community at UCR for
-          individuals interested in pursuing careers in therapy. Whether members
-          aspire to work in physical therapy, occupational therapy, counseling,
-          or even radiation therapy, our mission is to provide resources that
-          support academic and professional growth while helping members gain
-          clarity on their career goals. Driven by a passion to advocate for the
-          overall well-being of others, we strive to create a space where future
-          therapists across different fields can access valuable resources and
-          contribute to a collaborative, supportive environment.
-        </p>
-      </div>
-    </div>
+    <p className="text-ptso-purple-accent font-quicksand mx-auto h-auto w-5/6 text-center text-2xl">
+      We are committed to fostering a supportive community at UCR for
+      individuals interested in pursuing careers in therapy. Whether members
+      aspire to work in physical therapy, occupational therapy, counseling, or
+      even radiation therapy, our mission is to provide resources that support
+      academic and professional growth while helping members gain clarity on
+      their career goals. Driven by a passion to advocate for the overall
+      well-being of others, we strive to create a space where future therapists
+      across different fields can access valuable resources and contribute to a
+      collaborative, supportive environment.
+    </p>
   );
 };
 
