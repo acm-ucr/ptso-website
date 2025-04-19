ea9c9c9568055f938a214a7f333ad66410ea3247
Briana Garcia <briana_garcia_@outlook.com>
2025-04-18 20:30:48 -0700

    Add Our Founders section

---
+++ src/data/Founders.ts
@@ -0,0 +1,18 @@
+interface founder {
+  name: string;
+  img: string;
+  alt: string;
+}
+
+export const founders: founder[] = [
+  {
+    name: "Casey Min",
+    img: "/assets/board/CaseyMin.webp",
+    alt: "Casey Min",
+  },
+  {
+    name: "Natalie Hwang",
+    img: "/assets/board/NatalieHwang.webp",
+    alt: "Natalie Hwang",
+  },
+];
