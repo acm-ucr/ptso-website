a3547f7392c81eaff49b7c5e6a3dc25741431941
Rachel Chan <rchan2023@gmail.com>
2025-04-23 17:28:37 -0700

    fix

--- src/components/home/OurValuesText.tsx
+++ src/components/home/OurValuesText.tsx
@@ -7,14 +7,32 @@ const OurValuesText = () => {
   return (
     <div className="w-7/25">
       <div className="flex items-center">
-      <div className="flex relative w-20 h-20 place-items-center">
-        <Image src={yellowDiamond} alt="diamond" width={50} className=" z-5"/>
-        <Image src={blueStar1} alt="star" width={30} className="absolute left-6 top-11 z-10"/>
-        <Image src={blueStar2} alt="star2" width={15} className="absolute right-15 top-10 z-10"/>
+        <div className="relative flex h-20 w-20 place-items-center">
+          <Image src={yellowDiamond} alt="diamond" width={50} className="z-5" />
+          <Image
+            src={blueStar1}
+            alt="star"
+            width={30}
+            className="absolute top-11 left-6 z-10"
+          />
+          <Image
+            src={blueStar2}
+            alt="star2"
+            width={15}
+            className="absolute top-10 right-15 z-10"
+          />
+        </div>
+        <div className="font-beVietnamPro text-ptso-pink-accent mt-5 text-4xl font-extrabold">
+          Our Values
+        </div>
       </div>
-      <div className="mt-5 font-beVietnamPro text-ptso-pink-accent text-4xl font-extrabold">Our Values</div>
-      </div>
-      <p className="font-quicksand ml-5 mt-3">At PTSO, we are dedicated to fostering a supportive and inclusive community for students pursuing careers in therapy, emphasizing collaboration, professional growth, and lifelong learning. Our values center on advocacy, integrity, and empowerment, ensuring that every member has the resources and opportunities to succeed in their journey toward helping others.
+      <p className="font-quicksand mt-3 ml-5">
+        At PTSO, we are dedicated to fostering a supportive and inclusive
+        community for students pursuing careers in therapy, emphasizing
+        collaboration, professional growth, and lifelong learning. Our values
+        center on advocacy, integrity, and empowerment, ensuring that every
+        member has the resources and opportunities to succeed in their journey
+        toward helping others.
       </p>
     </div>
   );
