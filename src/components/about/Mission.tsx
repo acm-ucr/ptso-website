7f0bbd3c25d4c5402c071b459c3667c027157dc4
Briana Garcia <briana_garcia_@outlook.com>
2025-04-25 18:03:50 -0700

    Add About Us Mission

--- src/components/about/Mission.tsx
+++ src/components/about/Mission.tsx
@@ -1,4 +1,19 @@
+import Image from "next/image";
+import GroupPhoto from "@/public/assets/about/therapyAbout.webp";
+
 const Mission = () => {
-  return <div className="">Mission</div>;
+  return (
+    <div className="m-8 flex flex-col items-center justify-around gap-3 md:flex-row">
+      <Image src={GroupPhoto} alt="Group Photo" className="md:w-2/5" />
+      <div className="text-ptso-purple-accent text-center text-xs md:w-2/5 md:text-xl">
+        At PTSO, we are dedicated to fostering a supportive and inclusive
+        community for students pursuing careers in therapy, emphasizing
+        collaboration, professional growth, and lifelong learning. Our values
+        center on advocacy, integrity, and empowerment, ensuring that every
+        member has the resources and opportunities to succeed in their journey
+        toward helping others.
+      </div>
+    </div>
+  );
 };
 export default Mission;
