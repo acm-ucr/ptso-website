2fd47a17b2e6ed2d6d24a6341406c11499b5dad5
Yubin Zhen <yubinzhen@Yubins-MacBook-Pro.local>
2025-04-28 12:36:04 -0700

    added currentBoard

---
+++ src/components/Year.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface yearProps {
+  text: string;
+}
+
+const Year = ({ text }: yearProps) => {
+  return (
+    <div
+      className={
+        "text-ptso-pink-accent font-beVietnamPro mx-auto w-270 text-4xl font-bold"
+      }
+    >
+      {text}
+    </div>
+  );
+};
+
+export default Year;
--- src/components/board/CurrentBoard.tsx
+++ src/components/board/CurrentBoard.tsx
@@ -1,9 +1,24 @@
 import BoardMember from "@/components/board/BoardMember";
+import Year from "@/components/Year";
+import Image from "next/image";
+
+import currBoard from "@/public/assets/board/PTSOBoard1.webp";
 
 const CurrentBoard = () => {
   return (
-    <div className="">
-      <div>Current Board</div>
+    <div className="mt-20">
+      <Year text="'24-'25" />
+      <Image
+        src={currBoard}
+        alt="Image of current board"
+        className="mx-auto h-99 w-258 rounded-lg border-3"
+      />
+      <p className="font-average mx-auto mt-2 w-6/13 text-center">
+        (left to right): Casey Min, Erica Jwa, Dora Nguyen, Sage Kuck, Abigail
+        Hinojales, Koni Moss, Vallery Ayala, Montserrat Ramirez, Natalie Hwang,
+        Andrew Bach, (unpictured: Celena Fu)
+      </p>
+
       <BoardMember />
     </div>
   );
