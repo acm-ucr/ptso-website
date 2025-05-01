7420124e27ce31fa6532fc4de9043b181b397312
Kevin Loritsch <kloritsch58@gmail.com>
2025-04-30 20:46:53 -0700

    padding

--- src/components/about/TherapyCard.tsx
+++ src/components/about/TherapyCard.tsx
@@ -11,16 +11,16 @@ interface TherapyCardProps {
 const TherapyCard = ({ image, name, alt, color }: TherapyCardProps) => {
   return (
     <div
-      className={`${color} mx-auto flex w-3/4 flex-col items-center justify-between space-y-4 rounded-xl py-30`}
+      className={`${color} mx-auto flex w-3/4 flex-col items-center justify-between space-y-4 rounded-xl`}
     >
       <Image
         src={image}
         alt={alt}
         width={80}
         height={80}
-        className="object-contain"
+        className="mt-24 object-contain"
       />
-      <div className="font-beVietnamPro text-center text-xl text-white">
+      <div className="font-beVietnamPro mb-24 text-center text-xl text-white">
         {name}
       </div>
     </div>
