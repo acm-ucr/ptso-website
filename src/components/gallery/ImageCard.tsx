c90393f6498210e5bed1d0f3516d5c32ce3f5540
Briana Garcia <briana_garcia_@outlook.com>
2025-05-02 13:56:29 -0700

    Add Image Card

--- src/components/gallery/ImageCard.tsx
+++ src/components/gallery/ImageCard.tsx
@@ -1,7 +1,20 @@
-const ImageCard = () => {
+import { StaticImageData } from "next/image";
+import Image from "next/image";
+
+interface imagecard {
+  image: StaticImageData;
+  alt: string;
+  description: string;
+}
+
+const ImageCard = ({ image, alt, description }: imagecard) => {
   return (
-    <div className="">
-      <div>ImageCard</div>
+    <div className="group relative w-fit">
+      <div className="absolute z-1 h-full w-full rounded-2xl bg-black opacity-0 hover:opacity-35" />
+      <Image src={image} alt={alt}></Image>
+      <div className="absolute bottom-0 z-2 hidden p-4 text-xl text-white group-hover:block">
+        {description}
+      </div>
     </div>
   );
 };
