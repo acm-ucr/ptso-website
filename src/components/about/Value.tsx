ee12786bd8e25c0ac5ab1376c6b8e60e873703d3
Nam Nguyen <n0mn0m@Nams-Air.lan>
2025-05-02 18:17:38 -0700

    Background for values

--- src/components/about/Value.tsx
+++ src/components/about/Value.tsx
@@ -11,10 +11,12 @@ interface ValueProps {
 const Value = ({ name, image, alt, description }: ValueProps) => {
   return (
     <div className="flex w-7/8 gap-10 space-y-20 justify-self-center">
-      <Image src={image} alt={alt} />
+      <Image src={image} alt={alt} width={400} height={400} />
 
       <div className="flex-col space-y-7">
-        <div className="font-beVietnamPro text-3xl font-semibold">{name}</div>
+        <div className="font-quicksand text-3xl font-semibold text-ptso-purple-accent">
+          {name}
+        </div>
         <div className="font-quicksand text-ptso-purple-accent text-xl">
           {description}
         </div>
--- src/components/about/ValueList.tsx
+++ src/components/about/ValueList.tsx
@@ -1,9 +1,24 @@
 import Value from "@/components/about/Value";
 import { values } from "@/data/Values";
+import Image from "next/image";
+import TopWave from "@/public/assets/about/background/topWave.svg";
+import BottomWave from "@/public/assets/about/background/bottomWave.svg";
 
 const ValueList = () => {
   return (
-    <div className="flex-col">
+    <section className="relative w-full">
+      {/* Top Wave*/}
+      <div className="w-full">
+        <Image
+          src={TopWave}
+          alt="Decorative top wave"
+          width={1440}
+          height={200}
+          className="w-full object-cover"
+        />
+      </div>
+
+      <div className="flex-col px-8 py-24" style={{ backgroundColor: '#fbf3f3' }}>
       {values.map(({ name, image, alt, description }, index) => (
         <Value
           name={name}
@@ -14,6 +29,18 @@ const ValueList = () => {
         />
       ))}
     </div>
+
+      {/* Bottom Wave */}
+      <div className="w-full ">
+        <Image
+          src={BottomWave}
+          alt="Decorative bottom wave"
+          width={1440}
+          height={200}
+          className="w-full object-cover"
+        />
+      </div>
+    </section>
   );
 };
 export default ValueList;
