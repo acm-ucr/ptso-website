159fdf5be4cee90a6099544012c63970fe9c0896
Briana Garcia <briana_garcia_@outlook.com>
2025-05-02 18:22:28 -0700

    Small fixes to Image Card

--- src/components/gallery/ImageCard.tsx
+++ src/components/gallery/ImageCard.tsx
@@ -1,18 +1,18 @@
 import { StaticImageData } from "next/image";
 import Image from "next/image";
 
-interface imagecard {
+interface ImageCard {
   image: StaticImageData;
   alt: string;
   description: string;
 }
 
-const ImageCard = ({ image, alt, description }: imagecard) => {
+const ImageCard = ({ image, alt, description }: ImageCard) => {
   return (
     <div className="group relative w-fit">
-      <div className="absolute z-1 h-full w-full rounded-2xl bg-black opacity-0 hover:opacity-35" />
-      <Image src={image} alt={alt}></Image>
-      <div className="absolute bottom-0 z-2 hidden p-4 text-xl text-white group-hover:block">
+      <div className="absolute z-1 h-full w-full rounded-2xl bg-black opacity-0 transition-opacity duration-200 hover:opacity-35" />
+      <Image src={image} alt={alt} />
+      <div className="pointer-events-none absolute bottom-0 z-2 hidden p-4 text-xl text-white group-hover:block">
         {description}
       </div>
     </div>
