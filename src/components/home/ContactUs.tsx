f6e04b4a41677208e8a0daaf7a34a74575798b34
wesley <wesleylwu@gmail.com>
2025-05-05 16:09:42 -0700

    some mobile responsiveness

--- src/components/home/ContactUs.tsx
+++ src/components/home/ContactUs.tsx
@@ -32,13 +32,13 @@ const ContactUs = () => {
       ></iframe>
 
       <div className="relative w-screen">
-        <Image src={rectangle2} alt="Contact Us Wave" className="" />
+        <Image src={rectangle2} alt="Contact Us Wave" className="relative" />
 
-        <div className="absolute top-0 flex w-screen flex-row">
+        <div className="absolute top-0 flex w-screen md:flex-row">
           <div className="flex w-1/2 justify-center">
-            <div className="flex translate-x-[-8vh] translate-y-[40vh] flex-col items-center">
+            <div className=" md:translate-x-[-8vh] md:translate-y-[40vh]">
               <Title text="Contact Us" />
-              <Image src={contactUs} alt="Contact Us Hand" className="h-2/3" />
+              <Image src={contactUs} alt="Contact Us Hand" className="hidden md:block h-2/3" />
             </div>
           </div>
 
@@ -46,12 +46,12 @@ const ContactUs = () => {
             <Image
               src={vector6}
               alt="Contact Us Blob"
-              className="translate-x-[-8vh] translate-y-[26vh]"
+              className="hidden md:block translate-x-[-8vh] translate-y-[26vh]"
             />
 
-            <div className="bg-ptso-blue-light border-ptso-blue-accent absolute flex h-[72vh] w-[75vh] translate-x-[-5vh] translate-y-[35vh] items-center justify-center rounded-4xl border-3">
+            <div className="bg-ptso-blue-light border-ptso-blue-accent absolute flex h-auto w-full max-w-[75vh] md:h-[72vh] md:w-[75vh] translate-x-[-vh] md:translate-x-[-5vh] md:translate-y-[35vh] items-center justify-center rounded-4xl border-3">
               {isSubmitted ? (
-                <div className="font-average flex flex-col items-center justify-center gap-5 text-4xl">
+                <div className="font-average flex flex-col items-center gap-5 text-4xl">
                   <Image src={ptsoLogoLarge} alt="Logo" className="w-[30%]" />
                   Form Submitted!
                 </div>
@@ -62,37 +62,36 @@ const ContactUs = () => {
                   target="hidden_iframe"
                 >
                   <div>
-                    <div className="font-quicksand block text-3xl font-bold">
+                    <div className="font-quicksand block text-md md:text-3xl font-bold">
                       Name
                     </div>
                     <input
                       name="entry.1922039377"
                       type="text"
                       placeholder="Name"
-                      className="border-ptso-gray-primary mt-2 h-[5vh] w-[62vh] rounded-2xl border-1 bg-white p-3"
+                      className="border-ptso-gray-primary mt-2 h-[5vh] w-full md:w-[62vh] rounded-2xl border-1 bg-white p-3"
                       required
                     />
-                    <div className="font-quicksand mt-4 block text-3xl font-bold">
+                    <div className="font-quicksand mt-4 block text-md md:text-3xl font-bold">
                       Email
                     </div>
                     <input
                       name="entry.190572232"
                       type="text"
                       placeholder="Email"
-                      className="border-ptso-gray-primary mt-2 h-[5vh] w-[62vh] rounded-2xl border-1 bg-white p-3"
+                      className="border-ptso-gray-primary mt-2 h-[5vh] w-full md:w-[62vh] rounded-2xl border-1 bg-white p-3"
                       required
                     />
-                    <div className="font-quicksand mt-4 block text-3xl font-bold">
+                    <div className="font-quicksand mt-4 block text-md md:text-3xl font-bold">
                       Message
                     </div>
-                    <input
+                    <textarea
                       name="entry.1180546097"
-                      type="text"
-                      className="border-ptso-gray-primary mt-2 h-[19vh] w-full rounded-2xl border-1 bg-white p-3 pb-[20vh]"
+                      className="border-ptso-gray-primary mt-2 h-[19vh] w-full rounded-2xl border-1 bg-white p-3"
                       required
-                    />
+                    ></textarea>
                     <div className="mt-8 flex justify-center">
-                      <button className="bg-ptso-blue-dark h-[5vh] w-[15vh] rounded-2xl text-white">
+                      <button className="bg-ptso-blue-dark h-[5vh] w-[15vh] cursor-pointer rounded-2xl text-white">
                         Submit
                       </button>
                     </div>
