96f02e73c744e28ceb79fc0cdf5f5238ca64680c
sidechara <creativenamegoeshere0@gmail.com>
2025-05-18 12:50:24 -0700

    Animating values

--- src/components/about/ValueList.tsx
+++ src/components/about/ValueList.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Value from "@/components/about/Value";
 import { values } from "@/data/Values";
 import Image from "next/image";
+import { motion } from "motion/react";
+import Title from "@/components/Title";
 import TopWave from "@/public/assets/about/background/topWave.svg";
 import BottomWave from "@/public/assets/about/background/bottomWave.svg";
 
@@ -13,8 +16,18 @@ const ValueList = () => {
         className="w-full object-cover"
       />
 
+      
+
       <div className="-mt-4 flex-col bg-[#FFF4F4] px-8 pt-8">
+        <Title text="Our Values"/>
+        
         {values.map(({ name, image, alt, description }, index) => (
+          <motion.div
+          initial={{scale: 0, opacity: 0}}
+          whileInView={{scale: 1, opacity: 1}}
+          transition={{duration: 0.7, delay: index*0.05}}
+          viewport={{once: true}}
+          >
           <Value
             name={name}
             image={image}
@@ -22,6 +35,7 @@ const ValueList = () => {
             description={description}
             key={index}
           />
+          </motion.div>
         ))}
       </div>
 
