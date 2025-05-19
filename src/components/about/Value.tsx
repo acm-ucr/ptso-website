074ba2fca658ae1449fb06a4dca18c2dfb34e8cc
sidechara <creativenamegoeshere0@gmail.com>
2025-05-19 16:02:40 -0700

    Tweaks and mobile responsiveness

--- src/components/about/Value.tsx
+++ src/components/about/Value.tsx
@@ -10,11 +10,11 @@ interface ValueProps {
 
 const Value = ({ name, image, alt, description }: ValueProps) => {
   return (
-    <div className="flex w-7/8 gap-10 space-y-12 justify-self-center">
-      <Image src={image} alt={alt} />
+    <div className="flex w-7/8 gap-10 justify-self-center pb-12">
+      <Image src={image} alt={alt} className="sm-w-1/4 xl:w-1/10" />
 
-      <div className="flex-col space-y-5">
-        <div className="font-quicksand text-ptso-purple-accent text-3xl font-semibold">
+      <div className="sm-w-3/4 flex-col space-y-5 xl:w-9/10">
+        <div className="font-quicksand text-ptso-purple-accent text-2xl font-semibold md:text-3xl">
           {name}
         </div>
         <div className="font-quicksand text-ptso-purple-accent text-xl">
--- src/components/about/ValueList.tsx
+++ src/components/about/ValueList.tsx
@@ -16,25 +16,23 @@ const ValueList = () => {
         className="w-full object-cover"
       />
 
-      
-
       <div className="-mt-4 flex-col bg-[#FFF4F4] px-8 pt-8">
-        <Title text="Our Values"/>
-        
+        <Title text="Our Values" />
+
         {values.map(({ name, image, alt, description }, index) => (
           <motion.div
-          initial={{scale: 0.5, opacity: 0, y:0}}
-          whileInView={{scale: 1, opacity: 1, y:20}}
-          transition={{duration: 0.7, delay: index*0.05}}
-          viewport={{once: true}}
+            initial={{ scale: 0.6, opacity: 0, y: 0, x: 100 }}
+            whileInView={{ scale: 1, opacity: 1, y: 30, x: 0 }}
+            transition={{ duration: 0.8, delay: index * 0.05 }}
+            viewport={{ once: true }}
           >
-          <Value
-            name={name}
-            image={image}
-            alt={alt}
-            description={description}
-            key={index}
-          />
+            <Value
+              name={name}
+              image={image}
+              alt={alt}
+              description={description}
+              key={index}
+            />
           </motion.div>
         ))}
       </div>
