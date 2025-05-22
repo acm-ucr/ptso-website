cb87bfada00c77425123467c267c3d82d6d28ae1
wesley <wesleylwu@gmail.com>
2025-05-22 12:06:01 -0700

    add insta, logo, and blobs

--- src/components/home/Instagram.tsx
+++ src/components/home/Instagram.tsx
@@ -1,4 +1,31 @@
+"use client"
+
+import Image from "next/image";
+import circleLogo from "@/public/assets/home/circleLogo.webp"
+import vectorGroup from "@/public/assets/home/background/VectorGroup.svg"
+import { useEffect } from "react";
+
 const Instagram = () => {
-  return <div className="">Instagram</div>;
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = "https://w.behold.so/widget.js";
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
+  return (
+  <div className = "flex flex-row mb-20 bg-ptso-pink-primary w-screen h-screen">
+    <Image src={vectorGroup} alt = "Vector Group" className="absolute"></Image>
+      <div data-behold-id="4u02SoeevQeMHpLj9hs8" className=""></div>
+      <Image src = {circleLogo} alt = "Circle Logo" className="w-full scale-40">
+
+</Image>
+    </div>
+  );
 };
+
 export default Instagram;
