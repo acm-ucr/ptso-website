a6f25e329f38446470cd0150ff4421aa73161498
wesley <wesleylwu@gmail.com>
2025-05-22 20:22:51 -0700

    prettier

--- src/components/home/OurValuesText.tsx
+++ src/components/home/OurValuesText.tsx
@@ -5,11 +5,11 @@ import valueStarGroup from "@/public/assets/home/shapes/valueStarGroup.webp";
 const OurValuesText = () => {
   return (
     <>
-      <div className="mr-5 md:mr-0 flex items-center justify-center md:justify-start">
+      <div className="mr-5 flex items-center justify-center md:mr-0 md:justify-start">
         <Image src={valueStarGroup} alt="Stars" width={50} className="z-5" />
         <Title text="Our Values" />
       </div>
-<p className=" max-w-6xl md:w-full px-4 sm:px-6 md:px-8 font-quicksand mt-3 text-center text-sm sm:text-base md:text-left md:text-xl">
+      <p className="font-quicksand mt-3 max-w-6xl px-4 text-center text-sm sm:px-6 sm:text-base md:w-full md:px-8 md:text-left md:text-xl">
         At PTSO, we are dedicated to fostering a supportive and inclusive
         community for students pursuing careers in therapy, emphasizing
         collaboration, professional growth, and lifelong learning. Our values
