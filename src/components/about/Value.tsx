2d51278d315db12ee5ec66fb65201798b1697c53
sidechara <creativenamegoeshere0@gmail.com>
2025-05-22 23:09:03 -0700

    Mobile design

--- src/components/about/Value.tsx
+++ src/components/about/Value.tsx
@@ -10,13 +10,28 @@ interface ValueProps {
 
 const Value = ({ name, image, alt, description }: ValueProps) => {
   return (
-    <div className="flex w-7/8 gap-10 justify-self-center pb-12">
-      <Image src={image} alt={alt} className="w-1/5 md:w-1/7 xl:w-1/10" />
+    <div>
+      <div className="flex w-7/8 gap-10 justify-self-center pb-12 max-md:hidden">
+        <Image src={image} alt={alt} className="md:w-1/7 xl:w-1/10" />
 
-      <div className="w-4/5 flex-col space-y-5 md:w-6/7 xl:w-9/10">
-        <div className="font-quicksand text-ptso-purple-accent text-2xl font-semibold md:text-3xl">
-          {name}
+        <div className="flex-col space-y-5 md:w-6/7 xl:w-9/10">
+          <div className="font-quicksand text-ptso-purple-accent text-2xl font-semibold md:text-3xl">
+            {name}
+          </div>
+          <div className="font-quicksand text-ptso-purple-accent text-xl">
+            {description}
+          </div>
         </div>
+      </div>
+      <div className="flex w-7/8 flex-col gap-4 justify-self-center pb-12 md:hidden">
+        <div className="flex gap-6">
+          <Image src={image} alt={alt} className="w-1/11" />
+
+          <div className="font-quicksand text-ptso-purple-accent pt-2 text-2xl font-semibold">
+            {name}
+          </div>
+        </div>
+
         <div className="font-quicksand text-ptso-purple-accent text-xl">
           {description}
         </div>
