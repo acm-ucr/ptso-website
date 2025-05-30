b65c21ccae46d9667e4c9f3e065e8bd49c335c7b
Yubin Zhen <yubinzhen020105@gmail.com>
2025-05-30 13:10:00 -0700

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,12 +14,12 @@ const Footer = () => {
       className={`px-4 ${pathname == "/about" ? "bg-white" : pathname == "/gallery" || pathname == "/" ? "bg-ptso-blue-primary" : "bg-ptso-pink-light"}`}
     >
       <Divider />
-      <div className="flex flex-col items-center justify-center py-4 text-xl sm:text-2xl md:flex md:text-3xl">
+      <div className="flex py-4 text-xl sm:text-2xl flex-col items-center md:text-3xl lg:flex-row">
         <div className="flex items-center">
           <Image src={ptsoLogo} alt="Image" width={49} height={70} />
           <div className="px-4">PTSO</div>
         </div>
-        <div className="hidden grid-cols-2 justify-center space-x-5 divide-x-2 md:grid">
+        <div className="hidden grid-cols-2 space-x-5 divide-x-2 md:grid">
           <div className="flex flex-row items-center space-x-5">
             {footer.slice(0, 2).map(({ link, icon }, index) => (
               <Link
@@ -32,7 +32,7 @@ const Footer = () => {
               </Link>
             ))}
           </div>
-          <div className="item-center flex flex-col justify-center space-y-3 text-xl">
+          <div className="item-center flex flex-col  space-y-3 text-xl">
             {footer.slice(2).map(({ link, name }, index) => (
               <Link
                 className="transition ease-in-out hover:-translate-y-1"
