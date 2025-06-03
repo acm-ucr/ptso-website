99a17b56d9ca75d9e51d36935bb4ac197fa302da
Tony Liu <tonyliu05@hotmail.com>
2025-06-02 23:29:22 -0700

    Join us new Tab

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,6 +62,7 @@ const Navbar = () => {
           <Link
             href="https://www.instagram.com/ucr_ptso/"
             className="bg-ptso-pink-secondary hover:bg-ptso-pink-secondary/90 inline-block rounded-full px-6 py-2 text-white"
+            target="_blank"
           >
             JOIN US
           </Link>
