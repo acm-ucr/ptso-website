5aecaa09b29adf45ed72f706ff55c2fa759a363a
Kevin Pember <73316239+Kevin-Pember@users.noreply.github.com>
2026-03-26 19:05:10 -0700

    Combining Old Board and New Board Components into one data file

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
-import CurrentBoard from "@/components/board/CurrentBoard";
-import OldBoard from "@/components/board/OldBoard";
+import BoardCard from "@/components/board/BoardCard";
+import Boards from "@/data/Boards";
 import Founders from "@/components/board/Founders";
 import Image from "next/image";
 import waves from "@/public/assets/board/boardWaves.svg";
@@ -10,8 +10,15 @@ const Board = () => {
     <>
       <title>UCR PTSO - Board</title>
       <Header>Board</Header>
-      <CurrentBoard />
-      <OldBoard />
+      {Boards.map(({ year, image, position, board }, index) => (
+        <BoardCard
+          key={index}
+          year={year}
+          image={image}
+          position={position}
+          members={board}
+        />
+      ))}
       <Image src={waves} alt="Board Waves" className="w-full" />
       <Founders />
     </>
